91cc7d0a2b772191ef354882e074867cc7f50863
rbarakhvostov <rbarakhvostov@gmail.com>
2019-03-24 23:10:33 +0300

    my commit

--- src/node.js
+++ src/node.js
@@ -1,18 +1,30 @@
 class Node {
 	constructor(data, priority) {
-
+		this.data = data;
+		this.priority = priority;
+		this.parent = null;
+		this.left = null;
+		this.right = null;
 	}
 
 	appendChild(node) {
-
+		 
+		if (! this.left) this.left = node;
+		else if (this.left && this.right) {
+			this.left = this.left;
+			this.right = this.right;
+		}
+		else this.right = node;
 	}
 
 	removeChild(node) {
-
+		if (this.left === node) this.left = null;
+		else if (this.right === node) this.right = null;
+		else throw new Error();
 	}
 
 	remove() {
-
+		
 	}
 
 	swapWithParent() {
